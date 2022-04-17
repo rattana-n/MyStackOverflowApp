2eb6ad20cd5649d103aacd1dd85d23e22421339f
Rattana <rattana>
2022-04-17 09:35:12 -0700

    Initial Commit

---
+++ StackOverflowApp/StackOverflowAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StackOverflowAppApp.swift
+//  StackOverflowApp
+//
+//  Created by Rattana Noeun on 4/17/22.
+//
+
+import SwiftUI
+
+@main
+struct StackOverflowAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
